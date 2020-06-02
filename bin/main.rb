ea2d6d537b255b1bcf4124754ea539469fa1991c
vanheaven <vanheaven6@gmail.com>
2020-06-02 18:53:20 +0300

    Add test win state

--- bin/main.rb
+++ bin/main.rb
@@ -19,6 +19,17 @@ play_first = players[rand(2)]
 play_second = players.select { |elem| elem != play_first}
 puts "#{play_first} has been randomly chosen to play first"
 
+# test win states 
+win_states = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9 ], [3, 5, 7], [2, 5, 8]]
+player1 = [2, 4, 6]
+player2 = [1, 2, 3]
+
+if win_states.any? { |elem| elem.eql?(player2) }
+  puts "player 2 wins"
+else
+  puts "#{player2}"
+end
+
 
 # until Game.game_over?
 puts 'Choose one cell from the board between 1 and 9'
