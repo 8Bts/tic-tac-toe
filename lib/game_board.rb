c426b8a47a5e91a000bbb49795662f0cb50b5f31
8Bts <mcmizze@yahoo.com>
2020-06-03 20:01:30 +0400

    Completed mark_board method insie GameBoard class

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,19 +1,25 @@
 class GameBoard
   @@board_cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  @@input = 1
- 
+
+  def self.print_cells
+    p @@board_cells
+  end
+
   def self.print_board
-    "   #{@@board_cells[0]}  |  #{@@board_cells[1]}  |  #{@@board_cells[1]}  
-  ---- ----- -----  
-   #{@@board_cells[3]}  |  #{@@board_cells[4]}  |  #{@@board_cells[5]}  
-  ---- ----- -----  
+    "   #{@@board_cells[0]}  |  #{@@board_cells[1]}  |  #{@@board_cells[2]}
+  ---- ----- -----
+   #{@@board_cells[3]}  |  #{@@board_cells[4]}  |  #{@@board_cells[5]}
+  ---- ----- -----
    #{@@board_cells[6]}  |  #{@@board_cells[7]}  |  #{@@board_cells[8]} "
   end
-  
+
   def self.mark_board(index, sign)
-    
+    if index < 1 || index > 9
+      return 'Wrong index'
+    elsif @@board_cells[index - 1].is_a?(String)
+      return "The cell with index[#{index}] is already signed"
+    end
+
+    @@board_cells[index - 1] = sign
   end
 end
-
-puts GameBoard.mark_board(5, 'X')
-puts GameBoard.print_board
