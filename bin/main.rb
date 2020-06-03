8230ec15c2f492e81131654d4cbdc468637fac41
8Bts <mcmizze@yahoo.com>
2020-06-03 16:16:06 +0400

    Fix lint errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 
 puts 'Welcome!! to Tic Tac Toe, a game for geniuses'
 puts 'player 1: please enter your name'
@@ -5,44 +6,35 @@ p1_name = gets.chomp
 puts 'player 2: please enter your name'
 p2_name = gets.chomp
 
-#Board will be required from board.rb file
-puts '------Game board-------' 
-puts '  1  ||  2  ||  3  ||'
-puts ' ---    ---    ---'
-puts '  4  ||  5  ||  6  ||'
-puts ' ---    ---    ---'
-puts '  7  ||  8  ||  9 ||'
+# Board will be required from board.rb file
+puts '----Game board-----'
+puts '  1  ||  2  ||  3  '
+puts ' =================='
+puts '  4  ||  5  ||  6  '
+puts ' =================='
+puts '  7  ||  8  ||  9  '
 
 # Determine player to go first
 players = [p1_name, p2_name]
 play_first = players[rand(2)]
-play_second = players.select { |elem| elem != play_first}
+play_second_arr = players.reject { |elem| elem == play_first }
+play_second = play_second_arr[0]
 puts "#{play_first} has been randomly chosen to play first"
-
-# test win states 
-win_states = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9 ], [3, 5, 7], [2, 5, 8]]
-player1 = [2, 4, 6]
-player2 = [1, 2, 3]
-
-if win_states.any? { |elem| elem.eql?(player2) }
-  puts "player 2 wins"
-else
-  puts "#{player2}"
-end
-
 
 # until Game.game_over?
 puts 'Choose one cell from the board between 1 and 9'
 puts "#{play_first}'s turn"
 turn = gets.chomp.to_i
-#Validate turn variable
+puts "#{play_first} signed index #{turn} on board"
+# Validate turn variable
 # player1.pushturn(turn)
 # Game.won?(player1)
 # Update board
 # Second players turn to mark the board
-puts "#{play_second.join("")}'s turn'"
+puts "#{play_second.join('')}'s turn'"
 turn = gets.chomp.to_i
-#Validate turn variable
+puts "#{play_second} signed index #{turn} on board"
+# Validate turn variable
 # player2.pushturn(turn)
 # Game.won?(player2)
 # Update board
